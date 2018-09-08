7bce7359cccf8e1e788ddad7ab60e8653b9b1a16
anderson <anderson@zaruc.com.br>
2018-09-08 09:22:15 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='CaesarCipher',
+      version='0.1',
+      description='ROT-n encrypt and decrypt',
+      url='https://github.com/andersongomes001/CaesarCipher',
+      author='Anderson Gomes',
+      author_email='comprasgomes@hotmail.com',
+      license='MIT',
+      packages=['CaesarCipher'],
+      zip_safe=False)
